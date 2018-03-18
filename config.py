cc9b9b39605f905ae5858940cd864551938b83c6
Edgar Felizmenio <edgarfelizmenio@gmail.com>
2018-03-18 22:30:29 +0800

    host networking

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 db_user = 'root'
 db_password = 'password'
-db_host = 'database' # IP Address or hostname of where the MySQL database is deployed
-db_port = 3306
+db_host = 'localhost' # IP Address or hostname of where the MySQL database is deployed
+db_port = 3307
 db_name = 'raw_cr' # Name of the MySQL Databse
